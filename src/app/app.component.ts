84427b317ad539f702bd3d819ffe7449222d2b95
john clampet <jsclampet@gmail.com>
2023-05-10 18:54:12 -0500

    learning about ngClass directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  currentPage: number = 0;
   images = [
     {
       title: 'at the beach',
